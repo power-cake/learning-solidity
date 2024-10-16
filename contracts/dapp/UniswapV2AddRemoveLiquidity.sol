da7177cede160589d9696b2a25d40fc42cd18393
kakapo911 <kakapo911@hotmail.com>
2024-10-16 11:24:07 +0800

    ++

---
+++ contracts/dapp/UniswapV2AddRemoveLiquidity.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title x
+ * @dev   x
+ */
+contract TestUniswapLiquidity {
+    address private constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    function addLiquidity(
+        address _tokenA,
+        address _tokenB,
+        uint _amountA,
+        uint _amountB
+    ) external {
+        IERC20(_tokenA).transferFrom(msg.sender, address(this), _amountA);
+        IERC20(_tokenB).transferFrom(msg.sender, address(this), _amountB);
+
+        (uint amountA, uint amountB, uint liquidity) = IUniswapV2Router(ROUTER).addLiquidity(
+            _tokenA,
+            _tokenB,
+            _amountA,
+            _amountB,
+            1,
+            1,
+            address(this),
+            block.timestamp
+        );
+    }
+
+    function removeLiquidity(address _tokenA, address _tokenB) external {
+        address pair = IUniswapV2Factory(FACTORY).getPair(_tokenA, _tokenB);
+
+        uint liquidity = IERC20(pair).balanceOf(address(this));
+        IERC20(pair).approve(ROUTER, liquidity);
+
+        (uint amountA, uint amountB) = IUniswapV2Router(ROUTER).removeLiquidity(
+            _tokenA,
+            _tokenB,
+            liquidity,
+            1,
+            1,
+            address(this),
+            block.timestamp
+        );
+    }
+
+}
+
+interface IUniswapV2Router {
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    )
+    external
+    returns (
+        uint amountA,
+        uint amountB,
+        uint liquidity
+    );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+}
+
+interface IUniswapV2Factory {
+    function getPair(address token0, address token1) external view returns (address);
+}
